0ba6196dc41449de60ad2cc147df562c7865c800
rgonsahn <rgonsahn@yahoo.com>
2022-10-09 10:17:47 -0400

    created .env file

--- twilioUtils/index.js
+++ twilioUtils/index.js
@@ -1,5 +1,6 @@
 const express= require ('express')
 const client = require('twilio')('AC3b1d795d78f099f794b0d107a4c215ae','2426943a4ff0aab16bc89c4f0de486fd');
+require("dotenv").config();
 const app= express()
 const PORT= 3001 
 
@@ -8,7 +9,7 @@ app.get("/",(req,res)=>{
     sendMessage();
     res.send(`
     <div style="text align-center; padding-top:40px;";>
-    <h1> Welcome to the Polished Application</h1>
+    <h1> Thank you for using the Polished App</h1>
     <p> Please rate your experience </p>
     `)
 }) 
@@ -17,6 +18,14 @@ app.listen(PORT, () => {
 console.log(`Sample app listening at http://localhost:${PORT}`) 
 })
 
-function sendMessage(){
+function sendMessage(){ 
+    client.messages
+  .create({
+     body: 'Thank you for using the Polished App please, rate your experience',
+     from: '+16787838576',
+     to: '+16784128322'
+   })
+  .then(message => console.log(message.sid));
 
-}
+} 
+// console.log(process.env.twilio_api_key)
