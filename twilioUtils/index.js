fa820a4427e3cda16bb0a0cd8a26b3f151e34e5b
rgonsahn <rgonsahn@yahoo.com>
2022-10-08 22:23:01 -0400

    created sample page for reviews

--- twilioUtils/index.js
+++ twilioUtils/index.js
@@ -1,1 +1,15 @@
 const express= require ('express')
+const app= express()
+const PORT= 3001
+
+app.get("/",(req,res)=>{
+    res.send(`
+    <div style="text align-center; padding-top:40px;";>
+    <h1> Welcome to the Polished Application</h1>
+    <p> Please rate your experience </p>
+    `)
+}) 
+
+app.listen(PORT, () => {
+console.log(`Sample app listening at http://localhost:${PORT}`)
+})
