888e1163b851bfffb20857c73980f31f7c75611f
Arthur <odthientho@gmail.com>
2022-10-12 20:46:11 -0400

    holdon here

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const authenticationRoutes = require('./authentication-routes');
 
 router.use('/api', apiRoutes);
 router.use('', homeRoutes);
+router.use('', authenticationRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
