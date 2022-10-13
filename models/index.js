c6a87f4488f935dedcd0f3ab029ccfabf6bdca2e
Olivia Conley <oliviarconley@gmail.com>
2022-10-12 20:37:47 -0400

    added html to handlebars files for home & main

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const Services = require('./Services');
 const Appointment = require('./Appointment'); 
 const Appointment_service = require('./Appointment_service'); 
 
-//define the relationships
+//define the relationships - Arthur will do this 
 
 module.exports = { User, Services, Appointment, Appointment_service}; 
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Helpers for the app from nodemailer, twillio and auth (for login)
-const hbs = exphbs.create({ nodemailer, twillio, auth });
+const hbs = exphbs.create({ });
 
 const sess = {
   secret: 'Super secret secret',
