8f5a0ff916822fbf426101fe83b7abd02addd5ae
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-10-15 15:38:48 -0400

    styling added

--- controllers/index.js
+++ controllers/index.js
@@ -11,3 +11,26 @@ router.use((req, res) => {
 });
 
 module.exports = router;
+
+
+// const express = require('express');
+// //Loads the handlebars module
+// const handlebars = require('express-handlebars');
+// const app = express();
+// const port = 3000;
+// //Sets our app to use the handlebars engine
+// app.set('view engine', 'handlebars');
+// //Sets handlebars configurations (we will go through them later on)
+// app.engine('handlebars', handlebars.engine({
+// layoutsDir: __dirname + '/views/layouts',
+// }));
+// app.use(express.static('public'))
+// app.get('/', (req, res) => {
+// //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+// res.render('login', {layout : 'index'});
+// });
+// app.use(express.static("images"))
+// app.get("/", (req, res) => {
+//     res.render("login", {layout : 'index'});
+// });
+// app.listen(port, () => console.log(`App listening to port ${port}`));
